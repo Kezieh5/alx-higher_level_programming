a17f3506ce2be49f32776cd6088902b75aeb778c
Kezieh5 <mwangikezzieh@gmail.com>
2023-05-14 23:17:20 -0700

    prints all integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,5 @@
-def print_list_integer(my_list=None):
-    """Prints all the integers of a list.
-
-    Args:
-        my_list (list, optional): The list of integers. Defaults to None.
-
-    """
-    if my_list is None:
-        my_list = []
-
-    for i in my_list:
-        if isinstance(i, int):
-            print("{:d}".format(i))
-        else:
-            print("Error: List contains non-integer values.")
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Prints all integers of a list"""
+    for i in range(len(my_list)):
+        print('{:d}'.format(my_list[i]))
