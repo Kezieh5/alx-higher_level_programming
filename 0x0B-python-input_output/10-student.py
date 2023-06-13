24519d42bebbd80a1681c5d8bfce133e19035a0a
Kezieh5 <mwangikezzieh@gmail.com>
2023-06-13 00:58:31 -0700

    Student to disk and reload

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,5 +19,5 @@ class Student:
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {f: getattr(self, f) for f in attrs if hasattr(self, f)}
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
